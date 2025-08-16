1d9669fa6bc251e18825f7feecb60d23f9d3b55a
Ruslan.Sokolov <vaquero1master@gmail.com>
2025-08-16 16:53:01 +0200

    fix ExplorePage.jsx

--- src/pages/ExplorePage/ExplorePage.jsx
+++ src/pages/ExplorePage/ExplorePage.jsx
@@ -9,11 +9,14 @@ import { useEffect } from "react";
 
 import { Link } from "react-router-dom";
 
+import getPersistedUserIdFromToken from "../../shared/utils/getUserIdFromToken";
+
 import styles from "./ExplorePage.module.css";
 
 const ExplorePage = () => {
     const dispatch = useDispatch();
     const { allPosts, loading, error } = useSelector(getAllPostsSelector) || {};
+    const currentUserId = getPersistedUserIdFromToken();
 
     useEffect(() => {
         dispatch(getAllPosts());
@@ -21,7 +24,7 @@ const ExplorePage = () => {
 
     const shuffledPosts = [...allPosts].sort(() => Math.random() - 0.5).slice(0, 10);;
 
-    const elements = shuffledPosts.map((post, index) => <Link to={`/post-other/${post._id}`} key={post._id} className={index === 2 || index === 5 ? styles.tall : ""}>
+    const elements = shuffledPosts.map((post, index) => <Link to={post.author?._id?.toString() === currentUserId ? `/post-my/${post._id}` : `/post-other/${post._id}`} key={post._id} className={index === 2 || index === 5 ? styles.tall : ""}>
         <img src={post.image} alt="post" />
     </Link>);
 
